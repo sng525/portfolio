60abebb42b1b35898ce2473a9838ec4f2cf31260
Jiaojiao Song <ssssjulia525@gmail.com>
2024-10-22 21:26:49 +0200

    color design

--- app/page.js
+++ app/page.js
@@ -5,11 +5,18 @@ import ProjectSection from "./components/ProjectSection";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
 
+import { Montserrat_Alternates } from "next/font/google";
+
+const amatic_sc = Montserrat_Alternates({
+  subsets: ['cyrillic'],
+  weight: "400",
+})
+
 export default function Home() {
   return (
-      <main className="flex min-h-screen flex-col bg-[#060606]">
+      <main className={`${amatic_sc.className} flex min-h-screen flex-col bg-gray-100 dark:bg-gray-900`}>
         <Navbar/>
-        <div className="container mt-24 mx-auto px-12 py-12">
+        <div className="mt-24 mx-20 px-12 py-12">
           <HeroSection/>
           <AboutSection/>
           <ProjectSection/>
